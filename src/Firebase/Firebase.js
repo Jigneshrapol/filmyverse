192e54ebe2102678c0ac0e766453e781b6403a78
Jignesh Rapol <138034895+Jigneshrapol@users.noreply.github.com>
2024-02-11 17:37:17 +0530

    Update Firebase.js
    fixing errors

--- src/Firebase/Firebase.js
+++ src/Firebase/Firebase.js
@@ -5,12 +5,12 @@ import App from "../App";
 
 
 const firebaseConfig = {
-  apiKey:"",
-  authDomain:"",
-  projectId:"",
-  storageBucket:"",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyBFcF0s6h34PFGy8i1sHb_7l3curwm8PXI",
+  authDomain: "filmyverse-3d4a7.firebaseapp.com",
+  projectId: "filmyverse-3d4a7",
+  storageBucket: "filmyverse-3d4a7.appspot.com",
+  messagingSenderId: "160063526871",
+  appId: "1:160063526871:web:4960ff6300b0158dc19d92",
 };
 export const app = initializeApp(firebaseConfig);
 export const db= getFirestore(app);
